79a0d296136f5966a29d3a2af198339143a73d0f
Cobertos / Peter <me+git@cobertos.com>
2020-01-12 01:36:38 -0500

    chore: bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='md2notion',
-    version='1.0.1',
+    version='1.1.0',
     description='Utilities for importing Markdown files to Notion.so',
     long_description=open('README.md', 'r').read(),
     long_description_content_type="text/markdown",
